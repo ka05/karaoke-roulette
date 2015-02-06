29a1db96eb0db989a83ab5b462502079e70a72e5
Apple <apple@iOS-App-Challenge-MacBook.local>
2015-02-06 15:43:38 -0500

    Initial Commit

---
+++ KaraokeRoulette/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KaraokeRoulette
+//
+//  Created by Apple on 2/6/15.
+//  Copyright (c) 2015 Herendeen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
