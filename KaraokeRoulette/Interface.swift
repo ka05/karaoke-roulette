ecc457c3d82761d541d8a73b134a05c4c64d08f7
Apple <apple@iosappcemacbook.wireless.rit.edu>
2015-02-08 13:20:42 -0500

    integrating  kareokeVC into detail table view

--- KaraokeRoulette/Interface.swift
+++ KaraokeRoulette/Interface.swift
@@ -11,7 +11,7 @@ import UIKit
 
 let orangeColorSpecial = UIColor(red:0xe8/255, green:0x75/255,blue:0x18/255,alpha:1.0) // orange hex  = #e87518 - RGB (232, 117, 24)
 let blueColorSpecial = UIColor(red:0x18/255, green:0xc1/255,blue:0xe8/255,alpha:1.0) // blue hex = #18c1e8 - RGB (24, 193, 232)
-
+let charcoalColorSpecial = UIColor(red:0x5a/255, green:0x5a/255,blue:0x5a/255,alpha:1.0) // charcoal hex = #5a5a5a - RGB (24, 193, 232)
 class Interface: NSObject {
 
 }
